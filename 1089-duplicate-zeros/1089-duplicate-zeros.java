984d84c29c54562dd3aded64a917a34bdf344cfc
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2022-05-28 04:25:18 +0530

    Time: 1 ms (97.53%), Space: 46.2 MB (18.46%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,24 +1,24 @@
 class Solution {
-
     public void duplicateZeros(int[] arr) {
-        int r = arr.length - 2;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == 0) {
-                for (int j = r; j >= i; j--) {
-                    arr[j + 1] = arr[j];
-                }
-
-                arr[i] = 0;
-                i++;  // duplicated zero was not present in the original array so not used to duplicate again
+        int[] temp = new int[arr.length];
+        
+        int i =0;
+        int j =0;
+        while(i < temp.length){
+            if(arr[j] == 0){
+               temp[i] = 0;
+                i++;
+            }
+            if(i < temp.length){
+            temp[i++] = arr[j++];
             }
         }
+        
+        for(int k=0; k< temp.length; k++){
+            arr[k] = temp[k];
+        }
+        
+    
+        
     }
 }
-/**  
-  
-  1 2 0 4 5
-  1 2 0 0 4
-
-
-**/
