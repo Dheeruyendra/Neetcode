d79ef7b79cf278c5fb35a3437a528dc0d3e560da
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2022-05-28 16:25:06 +0530

    Time: 527 ms, Memory: 54.1 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    
+    
+    boolean isvalid(int[] arr , int index){
+        int l = index-1;
+        int r = index+1;
+        
+        while(l >= 0){
+            if(arr[l] >=arr[l+1]) return false;
+            l--;
+        }
+        
+        while(r < arr.length){
+            if( arr[r] >= arr[r-1]) return false;
+            r++;
+        }
+        
+        return true;
+    }
+    
+    
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+        
+       
+       for(int i=1; i<arr.length-1; i++){
+           
+           if(isvalid(arr, i)){
+               return true;
+           }
+           
+           
+       }
+        
+        return false;
+          
+        
+    }
+}
