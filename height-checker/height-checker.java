83cd0e1e309778ae59a0f0abbfd63937c67fabd3
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2022-07-03 04:53:14 +0530

    Time: 3 ms, Memory: 42.5 MB - LeetHub

---
+++ height-checker/height-checker.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        
+        int[] temp = heights.clone();
+        
+        Arrays.sort(temp);
+        
+        int count = 0;
+        
+        for(int i=0; i<temp.length; i++){
+             if(temp[i] != heights[i]){
+                 count+=1;
+             }
+        }
+        return count;
+        }
+}
