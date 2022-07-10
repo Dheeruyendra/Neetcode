16c2be1f7e183cab0923db7cd297abe7b2be2580
Dheeruyendra Pratap Singh <56476885+Dheeruyendra@users.noreply.github.com>
2022-07-10 08:31:32 +0530

    Time: 4 ms (36.91%), Space: 43.1 MB (6.41%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,33 +1,30 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
+    
+    
+       String trim(String s){
+           
           Stack<Character> s1 = new Stack<>();
-         Stack<Character> s2 = new Stack<>();
+           
+           
+           for(int i=0; i<s.length(); i++){
+               
+               if(s.charAt(i) != '#'){
+                   s1.push(s.charAt(i));
+               }else if(s1.size()!=0){
+                   s1.pop();
+               }
+               
+           }
+           
+           return s1.toString();
+           
+       }
+    
+    public boolean backspaceCompare(String s, String t) {
+         String s1 = trim(s);
+         String s2 = trim(t);
         
-        int i = 0;
-        int j = 0;
-        
-        while(i < s.length()){
-            
-            if( s.charAt(i) == '#'){
-               if(s1.size()>0) s1.pop();
-              
-            }else{
-                s1.push(s.charAt(i));
-            }
-            
-            i++;
-        }
-        
-        while( j < t.length()){
-            
-            if(  t.charAt(j) == '#'){
-              if(s2.size()>0)  s2.pop();
-            }else{
-                s2.push(t.charAt(j));
-            }
-            
-            j++;
-        }
+      
         
         return s1.equals(s2);
         
